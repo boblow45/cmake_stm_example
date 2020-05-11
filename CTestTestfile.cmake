a03a13a1ddbb69f8e46143c3c9ae9d08271e5364
Cillian OBrien <cillian.obrien@analog.com>
2020-05-11 23:59:24 +0100

    commenting out gtest code

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/github/stm_drone
+# Build directory: C:/github/stm_drone
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/github/stm_drone/test
+# Build directory: C:/github/stm_drone/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(ExampleTests "C:/github/stm_drone/test/ExampleTests.elf")
+set_tests_properties(ExampleTests PROPERTIES  _BACKTRACE_TRIPLES "C:/github/stm_drone/test/CMakeLists.txt;25;add_test;C:/github/stm_drone/test/CMakeLists.txt;0;")
