b6c03b419b82bc9ad17897a0e98ff8c8823f849c
Cillian O'Brien <cillianobrien01@gmail.com>
2021-02-14 23:51:53 +0000

    updating code so flashing tool works on linux

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# Install tools to compile code
+sudo apt-get install gcc-arm-none-eabi binutils-arm-none-eabi openocd
+
+# will install arm gdb and can be accessed using the following
+# arm-none-eabi-gdb
+sudo apt install gdb-multiarch
+ln -s /usr/bin/gdb-multiarch /usr/bin/arm-none-eabi-gdb
