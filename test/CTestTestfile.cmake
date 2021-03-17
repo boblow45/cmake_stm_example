d9eb8f223d7af0100112d894cc9ac58333672b20
Cillian O'Brien <cillianobrien01@gmail.com>
2021-03-17 21:29:28 +0000

    working version of googletest

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/github/stm_drone/test
-# Build directory: C:/github/stm_drone/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ExampleTests "C:/github/stm_drone/test/ExampleTests.elf")
-set_tests_properties(ExampleTests PROPERTIES  _BACKTRACE_TRIPLES "C:/github/stm_drone/test/CMakeLists.txt;25;add_test;C:/github/stm_drone/test/CMakeLists.txt;0;")
